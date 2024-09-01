2780ce2a4c4ca13cb20c32a8d3ffe371ce0b3833
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-01 16:21:54 -0400

    Set initial date

---
+++ src/modules/form/submit.js
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+
+const form = document.querySelector("form");
+const selectedDate = document.getElementById("date");
+
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
