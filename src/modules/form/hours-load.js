316a2041e6dd3b1791baf3538123130a4c0b6709
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-01 20:36:56 -0400

    Change style on select hour

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours.js";
+import { hoursClick } from "./hours-click.js";
 
 const hours = document.getElementById("hours");
 
@@ -29,6 +30,8 @@ export function hoursLoad({ date }) {
     }
     hours.append(li);
   });
+  //Adiciona o evento de click nos horários disponíveis
+  hoursClick();
 }
 
 function hourHeaderAdd(title) {
