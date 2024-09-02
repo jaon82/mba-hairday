8754f1191da3a32ff8f86860d0f0e2019c3ab9f8
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-01 20:50:44 -0400

    Mount schedule object on submit

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 selectedDate.value = inputToday;
@@ -9,4 +10,28 @@ selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+  try {
+    //Recuperar o nome do cliente
+    const name = clientName.value.trim();
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+    //Recuperar o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+    if (!hourSelected) {
+      return alert("Selecione a hora!");
+    }
+    const [hour] = hourSelected.innerText.split(":");
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+    //Gerar ID do agendamento
+    const id = new Date().getTime();
+    const schedule = {
+      id,
+      name,
+      when,
+    };
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+    console.log(error);
+  }
 };
