8c48c45257ed4c4ac64753faec27856f64c83808
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-03 19:54:21 -0400

    Load schedules by date

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleShow } from "./show.js";
 
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtém a data do input
   const date = selectedDate.value;
+  //Busca na API os agendamentos do dia
+  const dailySchedules = await scheduleFetchByDay({ date });
+  scheduleShow({ dailySchedules });
   //Renderiza as horas disponíveis
   hoursLoad({ date });
 }
